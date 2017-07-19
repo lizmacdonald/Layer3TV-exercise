c6c1450b9702ad1ea55412ed9cbb54db8f7ffc32
Liz M <lizmacdonald45@gmail.com>
2017-07-18 19:44:11 -0600

    read in data, dataset setup

--- exercise.py
+++ exercise.py
@@ -1,1 +1,15 @@
-### 
+#!/usr/bin/env python2
+
+#############################################################
+# Layer3 Exercise
+#############################################################
+
+import openpyxl as px
+import pandas as pd
+
+dat = px.load_workbook('subscriber-data.xlsx', data_only=True, parse_dates = [0])
+ws = dat['data']
+
+df = pd.DataFrame(ws.values)
+df.columns = [df.iloc[0]]
+df = df.drop([0, ])
