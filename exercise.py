62ea81f3991039ddc684a6d5451d5aa092fa29d1
Liz M <lizmacdonald45@gmail.com>
2017-07-18 23:16:17 -0600

    set up week numbering system

--- exercise.py
+++ exercise.py
@@ -6,10 +6,38 @@
 
 import openpyxl as px
 import pandas as pd
+import datetime
 
-dat = px.load_workbook('subscriber-data.xlsx', data_only=True, parse_dates = [0])
+
+dat = px.load_workbook('subscriber-data.xlsx', data_only=True)
 ws = dat['data']
 
 df = pd.DataFrame(ws.values)
 df.columns = [df.iloc[0]]
 df = df.drop([0, ])
+
+dat_form = "%Y-%m-%d %H:%S:%M"
+
+## Set up week number column
+
+max_day = pd.to_datetime(df.iloc[1,0], format = dat_form)
+days = df.iloc[:,0]
+
+wk = []
+
+for day in days:
+	wk.append(pd.to_datetime(day, format = dat_form))
+
+dif = []
+
+for day in wk:
+	dif.append(int((max_day - day).days))
+
+wknum = []
+
+for i in dif:
+	wknum.append(i / 7)
+
+df['dif'] = wknum
+
+
