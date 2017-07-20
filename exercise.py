5e9d54c1e5909a0fa4e6d1f1b1decf9392e09a6b
Liz M <lizmacdonald45@gmail.com>
2017-07-19 21:53:14 -0600

    set up column names in proper format

--- exercise.py
+++ exercise.py
@@ -7,8 +7,10 @@
 import openpyxl as px
 import pandas as pd
 import datetime
+import numpy as np
+import transposer
 
-
+## read in data
 dat = px.load_workbook('subscriber-data.xlsx', data_only=True)
 ws = dat['data']
 
@@ -16,28 +18,40 @@ df = pd.DataFrame(ws.values)
 df.columns = [df.iloc[0]]
 df = df.drop([0, ])
 
-dat_form = "%Y-%m-%d %H:%S:%M"
 
 ## Set up week number column
-
+### need to find the min date so week 1 is the first week not the last week
+dat_form = "%Y-%m-%d %H:%S:%M"
 max_day = pd.to_datetime(df.iloc[1,0], format = dat_form)
 days = df.iloc[:,0]
 
 wk = []
-
-for day in days:
-	wk.append(pd.to_datetime(day, format = dat_form))
-
 dif = []
-
-for day in wk:
-	dif.append(int((max_day - day).days))
-
 wknum = []
 
+for d in days:
+	wk.append(pd.to_datetime(d, format = dat_form))
+
+for w in wk:
+	dif.append(int((max_day - w).days))
+
+## need to add a 1 to the wk number so the week doesn't start at zero
 for i in dif:
 	wknum.append(i / 7)
 
-df['dif'] = wknum
 
+df['dif'] = wknum
+df['dif'] = 'Week ' + df['dif'].astype(str)
+
+
+## Set up column names to be the format I want
+df.columns = ['activity_date', 'market', 'Total Connects', 'Total Disconnects', 'Self Installs', 'Pro Installs', 'Disconnects', 'Post Install Returns', 'Total Subscribers', 'dif']
+
+df['Net Gain'] = df['Total Connects'].astype(int) - df['Total Disconnects'].astype(int)
+
+df['Beginning Sub']
+
+
+
+dfout = pd.transpose(df)
 
